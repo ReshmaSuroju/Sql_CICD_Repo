484d7843d54ad62867d42e296577e5e4ec59548b
Reshma <reshma.suroju@websynergies.com>
2023-11-08 15:22:32 +0800

    New Date column added in Feedback table

--- CICD_DB/dbo/Tables/Table_DevSQL_Feedback.sql
+++ CICD_DB/dbo/Tables/Table_DevSQL_Feedback.sql
@@ -1,6 +1,7 @@
 ﻿CREATE TABLE [dbo].[Table_DevSQL_Feedback] (
     [Name]    NVARCHAR (50) NOT NULL,
     [Comment] NVARCHAR (50) NOT NULL,
-    [Email]   NVARCHAR (50) NOT NULL
+    [Email]   NVARCHAR (50) NOT NULL, 
+    [Date] DATETIME NULL
 );
 
