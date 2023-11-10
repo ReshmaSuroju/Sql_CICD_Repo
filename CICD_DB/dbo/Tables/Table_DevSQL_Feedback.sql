4b3857f96dafc3829876e43751f783acac75aeea
Reshma <reshma.suroju@websynergies.com>
2023-11-10 15:24:11 +0800

    Adding column Date2

--- CICD_DB/dbo/Tables/Table_DevSQL_Feedback.sql
+++ CICD_DB/dbo/Tables/Table_DevSQL_Feedback.sql
@@ -2,6 +2,7 @@
     [Name]    NVARCHAR (50) NOT NULL,
     [Comment] NVARCHAR (50) NOT NULL,
     [Email]   NVARCHAR (50) NOT NULL, 
-    [Date] DATETIME NULL
+    [Date] DATETIME NULL, 
+    [Date2] DATETIME NOT NULL
 );
 
